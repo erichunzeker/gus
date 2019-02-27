79c0eabf90dede54d0f21b5de6fcd88218f14c1e
Eric Hunzeker <emh128@pitt.edu>
2019-02-27 13:18:27 -0500

    update home

--- app.py
+++ app.py
@@ -13,15 +13,20 @@ from models import User
 spotify_secret = os.environ.get('SPOTIFY_SECRET')
 spotify_id = os.environ.get('SPOTIFY_ID')
 
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
 @app.route('/add/')
 def webhook():
     name = "ram"
     email = "ram@ram.com"
-    u = User(nickname = name, email = email)
+    u = User(nickname=name, email=email)
     print("user created", u)
     db.session.add(u)
     db.session.commit()
     return "user created"
+
 
 @app.route('/delete/')
 def delete():
@@ -29,6 +34,7 @@ def delete():
     db.session.delete(u)
     db.session.commit()
     return "user deleted"
+
 
 if __name__ == '__main__':
     app.run()
