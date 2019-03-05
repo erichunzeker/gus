53e99a736c181dedcf81da6403a0e4c6934bd818
Brian Choromanski <BrianChoromanski@gmail.com>
2019-03-05 12:01:17 -0500

    Added song table and

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 from manage import db,app
 
 class User(db.Model):
-    __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
 
-    def __repr__(self):
-        return '<User %r>' % (self.nickname)
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(8), nullable=False)
+    spotifyid = db.Column(db.String(22))
+    #Other identifiers that we might need
