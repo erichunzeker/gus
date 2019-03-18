ce3c534d5eea00747cdc9ee1563d67288f098e38
Eric Hunzeker <emh128@pitt.edu>
2019-03-18 14:41:32 -0400

    layout is messed up but got spotipy to work

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 import os
+import json, requests
+import spotipy
+import spotipy.oauth2 as oauth2
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
@@ -13,9 +17,22 @@ from models import User
 spotify_secret = os.environ.get('SPOTIFY_SECRET')
 spotify_id = os.environ.get('SPOTIFY_ID')
 
+
+credentials = oauth2.SpotifyClientCredentials(
+        client_id=spotify_id,
+        client_secret=spotify_secret)
+
+token = credentials.get_access_token()
+spotify = spotipy.Spotify(auth=token)
+
+
+
 @app.route('/')
 def homepage():
-    return render_template('home.html')
+    name="one dance"
+    results = spotify.search(q='track:' + name, type='track', limit=1)
+    print(results)
+    return render_template('home.html', data=results['tracks']['items'][0]['album']['images'][1]['url'])
 
 @app.route('/add/')
 def webhook():
