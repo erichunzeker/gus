6c45258b21073e3f38b74e116329e6c70ad33545
Eric Hunzeker <emh128@pitt.edu>
2019-03-20 12:39:45 -0400

    stuffs heating up. we got links coming from all directions baby

--- app.py
+++ app.py
@@ -19,14 +19,12 @@ from models import User, Song
 spotify_secret = os.environ.get('SPOTIFY_SECRET')
 spotify_id = os.environ.get('SPOTIFY_ID')
 
-
 credentials = oauth2.SpotifyClientCredentials(
-        client_id=spotify_id,
-        client_secret=spotify_secret)
+    client_id=spotify_id,
+    client_secret=spotify_secret)
 
 token = credentials.get_access_token()
 spotify = spotipy.Spotify(auth=token)
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -38,7 +36,7 @@ def homepage():
         results = spotify.search(q=toggle + ':' + name, type=toggle, limit=10)
         count = 0
         data = []
-        #print(results)
+        # print(results)
 
         if toggle == 'track':
             for i in results['tracks']['items']:
@@ -49,9 +47,9 @@ def homepage():
                                  "spotifyid": results['tracks']['items'][count]['id']})
                 else:
                     data.append({"img": results['tracks']['items'][count]['album']['images'][0]['url'],
-                                "name": results['tracks']['items'][count]['name'],
-                                "artist": results['tracks']['items'][count]['album']['artists'][0]['name'],
-                                "spotifyid": results['tracks']['items'][count]['id']})
+                                 "name": results['tracks']['items'][count]['name'],
+                                 "artist": results['tracks']['items'][count]['album']['artists'][0]['name'],
+                                 "spotifyid": results['tracks']['items'][count]['id']})
                 count += 1
 
         elif toggle == 'artist':
@@ -62,8 +60,8 @@ def homepage():
                                  "artist": results['artists']['items'][count]['name']})
                 else:
                     data.append({"img": results['artists']['items'][count]['images'][0]['url'],
-                                "name": results['artists']['items'][count]['name'],
-                                "artist": results['artists']['items'][count]['name']})
+                                 "name": results['artists']['items'][count]['name'],
+                                 "artist": results['artists']['items'][count]['name']})
                 count += 1
         else:
             for i in results['albums']['items']:
@@ -74,21 +72,23 @@ def homepage():
                                  "spotifyid": results['albums']['items'][count]['id']})
                 else:
                     data.append({"img": results['albums']['items'][count]['images'][0]['url'],
-                                "name": results['albums']['items'][count]['name'],
-                                "artist": results['albums']['items'][count]['artists'][0]['name'],
-                                "spotifyid": results['albums']['items'][count]['id']})
+                                 "name": results['albums']['items'][count]['name'],
+                                 "artist": results['albums']['items'][count]['artists'][0]['name'],
+                                 "spotifyid": results['albums']['items'][count]['id']})
                 count += 1
-        #print(data)
+        # print(data)
         # make dict with limit of ten: img, name, artist
         # [ {img, name, artist}, {img, name, artist} ]
         return render_template('home.html', data=data, type=toggle)
     return render_template('home.html', error=error)
 
+
 def generateKey():
     key = secrets.token_urlsafe(6)
-    while(db.session.query(Song).filter(Song.url == key).count() != 0):
-       key = secrets.token_urlsafe(6)
+    while db.session.query(Song).filter(Song.url == key).count() != 0:
+        key = secrets.token_urlsafe(6)
     return key
+
 
 @app.route('/create/<type>/<spotifyid>')
 def create(type, spotifyid):
@@ -101,13 +101,15 @@ def create(type, spotifyid):
     db.session.commit()
     return redirect(url_for('load', url=key))
 
+
 @app.route('/s/<url>')
 def load(url):
     song = db.session.query(Song).filter(Song.url == url)
-    if(song.count() == 0):
+    if song.count() == 0:
         return "404 url not in database"
     else:
-        return '<a href="https://open.spotify.com/' + song[0].type + '/'+ song[0].spotifyid+'">https://open.spotify.com/' + song[0].type + '/'+ song[0].spotifyid+'</a>'
+        links = [{'spotify': ('https://open.spotify.com/' + song[0].type + '/' + song[0].spotifyid)}]
+        return render_template('landing.html', link=links)
 
 if __name__ == '__main__':
     app.run()
