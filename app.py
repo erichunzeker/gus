52e623628dcc2999c1ece1fce806770fab937652
Eric Hunzeker <emh128@pitt.edu>
2019-03-20 16:21:19 -0400

    updated result page

--- app.py
+++ app.py
@@ -33,52 +33,8 @@ def homepage():
     if request.method == 'POST':
         name = request.form['name']
         toggle = request.form['toggle']
-        results = spotify.search(q=toggle + ':' + name, type=toggle, limit=10)
-        count = 0
-        data = []
-        # print(results)
+        data = getdata(toggle, name)
 
-        if toggle == 'track':
-            for i in results['tracks']['items']:
-                if len(results['tracks']['items'][count]['album']['images']) == 0:
-                    data.append({"img": "static/img/note.png",
-                                 "name": results['tracks']['items'][count]['name'],
-                                 "artist": results['tracks']['items'][count]['album']['artists'][0]['name'],
-                                 "spotifyid": results['tracks']['items'][count]['id']})
-                else:
-                    data.append({"img": results['tracks']['items'][count]['album']['images'][0]['url'],
-                                 "name": results['tracks']['items'][count]['name'],
-                                 "artist": results['tracks']['items'][count]['album']['artists'][0]['name'],
-                                 "spotifyid": results['tracks']['items'][count]['id']})
-                count += 1
-
-        elif toggle == 'artist':
-            for i in results['artists']['items']:
-                if len(results['artists']['items'][count]['images']) == 0:
-                    data.append({"img": "static/img/note.png",
-                                 "name": results['artists']['items'][count]['name'],
-                                 "artist": results['artists']['items'][count]['name']})
-                else:
-                    data.append({"img": results['artists']['items'][count]['images'][0]['url'],
-                                 "name": results['artists']['items'][count]['name'],
-                                 "artist": results['artists']['items'][count]['name']})
-                count += 1
-        else:
-            for i in results['albums']['items']:
-                if len(results['albums']['items'][count]['images']) == 0:
-                    data.append({"img": "static/img/note.png",
-                                 "name": results['albums']['items'][count]['name'],
-                                 "artist": results['albums']['items'][count]['artists'][0]['name'],
-                                 "spotifyid": results['albums']['items'][count]['id']})
-                else:
-                    data.append({"img": results['albums']['items'][count]['images'][0]['url'],
-                                 "name": results['albums']['items'][count]['name'],
-                                 "artist": results['albums']['items'][count]['artists'][0]['name'],
-                                 "spotifyid": results['albums']['items'][count]['id']})
-                count += 1
-        # print(data)
-        # make dict with limit of ten: img, name, artist
-        # [ {img, name, artist}, {img, name, artist} ]
         return render_template('home.html', data=data, type=toggle)
     return render_template('home.html', error=error)
 
@@ -92,6 +48,12 @@ def generateKey():
 
 @app.route('/create/<type>/<spotifyid>')
 def create(type, spotifyid):
+    print('here')
+    song = db.session.query(Song).filter(Song.spotifyid == spotifyid and Song.type == type)
+    print(song.count())
+    if song.count() != 0:
+        return redirect(url_for('load', url=song[0].url))
+
     key = generateKey()
     print("HERE")
     print(key)
@@ -108,8 +70,67 @@ def load(url):
     if song.count() == 0:
         return "404 url not in database"
     else:
+        # v v v pass in links to this dictionary list v v v
         links = [{'spotify': ('https://open.spotify.com/' + song[0].type + '/' + song[0].spotifyid)}]
-        return render_template('landing.html', link=links)
+        data = fetchattributes(song[0].type, song[0].spotifyid)
+        return render_template('landing.html', link=links, data=data, url=url)
+
+
+def fetchattributes(type, id):
+    if type == 'track':
+        return spotify.track(id)
+    elif type == 'album':
+        return spotify.album(id)
+    else:
+        return spotify.artist(id)
+
+
+def getdata(toggle, query):
+    count = 0
+    data = []
+    results = spotify.search(q=toggle + ':' + query, type=toggle, limit=15)
+
+    if toggle == 'track':
+        for i in results['tracks']['items']:
+            if len(results['tracks']['items'][count]['album']['images']) == 0:
+                data.append({"img": "static/img/note.png",
+                             "name": results['tracks']['items'][count]['name'],
+                             "artist": results['tracks']['items'][count]['album']['artists'][0]['name'],
+                             "spotifyid": results['tracks']['items'][count]['id']})
+            else:
+                data.append({"img": results['tracks']['items'][count]['album']['images'][0]['url'],
+                             "name": results['tracks']['items'][count]['name'],
+                             "artist": results['tracks']['items'][count]['album']['artists'][0]['name'],
+                             "spotifyid": results['tracks']['items'][count]['id']})
+            count += 1
+
+    elif toggle == 'artist':
+        for i in results['artists']['items']:
+            if len(results['artists']['items'][count]['images']) == 0:
+                data.append({"img": "static/img/note.png",
+                             "name": results['artists']['items'][count]['name'],
+                             "artist": results['artists']['items'][count]['name']})
+            else:
+                data.append({"img": results['artists']['items'][count]['images'][0]['url'],
+                             "name": results['artists']['items'][count]['name'],
+                             "artist": results['artists']['items'][count]['name']})
+            count += 1
+    else:
+        for i in results['albums']['items']:
+            if len(results['albums']['items'][count]['images']) == 0:
+                data.append({"img": "static/img/note.png",
+                             "name": results['albums']['items'][count]['name'],
+                             "artist": results['albums']['items'][count]['artists'][0]['name'],
+                             "spotifyid": results['albums']['items'][count]['id']})
+            else:
+                data.append({"img": results['albums']['items'][count]['images'][0]['url'],
+                             "name": results['albums']['items'][count]['name'],
+                             "artist": results['albums']['items'][count]['artists'][0]['name'],
+                             "spotifyid": results['albums']['items'][count]['id']})
+            count += 1
+
+    return data
+
 
 if __name__ == '__main__':
     app.run()
